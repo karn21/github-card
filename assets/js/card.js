d6c01ae05e5c6f1bf39f04375a3279dba15cfae7
karn21 <karn212000@gmail.com>
2020-04-05 09:17:43 +0530

    added input

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,37 +1,49 @@
-  var user = document.getElementById("github-profile-card").getAttribute('github-user')
-
-  // const user = "karn21"
-  var url = `https://api.github.com/users/${user}`
-  fetch(url)
-  .then((resp) => {
-    if (resp.status == 404) {
-      console.log("not found")
-      // 
+  document.getElementById("generate-profile").onclick = function(){
+    const user = document.getElementById("github-user-input").value
+    if(user !== "") {
+      document.getElementById("github-profile-card").setAttribute('github-user',user)
+      generateProfile()
+    } else {
+      alert("Enter a username.")
     }
-    return resp.json()
-  }) 
-  .then(function(data) {
-  	
-  	document.getElementById("github-name").innerHTML = data.name
-    document.getElementById("github-user-name").innerHTML = `(@${data.login})`
-  	document.getElementById("github-name").href = data.html_url
-    document.getElementById("github-avatar").src = data.avatar_url
-  	document.getElementById("github-bio").innerHTML = data.bio
-  	var following = document.getElementById("github-following")
-  	following.innerHTML = data.following
-  	following_url = data.html_url + "/following"
-  	following.parentElement.href = following_url
-  	var followers = document.getElementById("github-followers")
-  	followers.innerHTML = data.followers
-  	followers_url = data.html_url + "/followers"
-  	followers.parentElement.href = followers_url
-  	var repos = document.getElementById("github-repos")
-  	repos.innerHTML = data.public_repos
-  	repos_url = data.html_url + "?tab=repositories"
-  	repos.parentElement.href = repos_url
-    console.log(data)
+    
+  }
+
+
+  function generateProfile() {
+    var user = document.getElementById("github-profile-card").getAttribute('github-user')
+    var url = `https://api.github.com/users/${user}`
+    fetch(url)
+    .then((resp) => {
+      if (resp.status == 404) {
+        console.log("not found")
+        // 
+      }
+      return resp.json()
+    }) 
+    .then(function(data) {
+      
+      document.getElementById("github-name").innerHTML = data.name
+      document.getElementById("github-user-name").innerHTML = `(@${data.login})`
+      document.getElementById("github-name").href = data.html_url
+      document.getElementById("github-avatar").src = data.avatar_url
+      document.getElementById("github-bio").innerHTML = data.bio
+      var following = document.getElementById("github-following")
+      following.innerHTML = data.following
+      following_url = data.html_url + "/following"
+      following.parentElement.href = following_url
+      var followers = document.getElementById("github-followers")
+      followers.innerHTML = data.followers
+      followers_url = data.html_url + "/followers"
+      followers.parentElement.href = followers_url
+      var repos = document.getElementById("github-repos")
+      repos.innerHTML = data.public_repos
+      repos_url = data.html_url + "?tab=repositories"
+      repos.parentElement.href = repos_url
+      console.log(data)
+      })
+    .catch((error) => {
+      console.log(error)
     })
-  .catch((error) => {
-    console.log(error)
-  })
+  }
 
